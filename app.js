a272b17b51f021e19ddb54ad124b946c942519db
Yangesh <80026521+yangeshwar@users.noreply.github.com>
2021-07-21 14:14:23 +0530

    matrix changes

--- app.js
+++ app.js
@@ -2,10 +2,10 @@
 product();
 
 function product(){
-  var productName = ['','Pink T-shirt','Yellow Hoodie','Black T-shirt','Black Hoodie'];
-  var productPrice = ['','$25','$36','$25','$42'];
+  var productName = ['','Pink T-shirt','Yellow Hoodie','Black T-shirt','Black Hoodie','Black Hoodie','Black Hoodie','Black Hoodie'];
+  var productPrice = ['','$25','$36','$25','$42','$42','$42','$42'];
     var product = document.getElementById('product');
-    for(let i = 1; i < 5; i++ ){
+    for(let i = 1; i < 8; i++ ){
       product.innerHTML += '<div id="prd'+i+'" class="product">'+
       '<div class="product-img">'+
           '<img src="https://cdn.cartup.ai/web/sample/'+i+'.jpg" alt="">'+
